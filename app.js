8183a4c655a9056e870467c5d664baec80ef15a8
ps_Thant <pyaesone.ii224@gmail.com>
2021-11-11 21:03:35 +0630

    minokca continuation update

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express= require("express");
+const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index");
 
-
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(indexRoutes);
 
-
-app.listen(process.env.PORT, process.env.IP, ()=>{
-    console.log("Myanmar Nwe server is running");
-}) 
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log("Myanmar Nwe server is running");
+});
